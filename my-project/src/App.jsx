c13a94dd3db428beedf234ee0d4b0e42941c3fc9
Codebox124 <rajcodes733@gmail.com>
2023-03-11 10:03:27 -0800

    blog section done

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import Hero from "./components/Hero"
 import Header from "./components/Header"
 import LatestBlog from "./components/LatestBlog"
+import Latestcourses from "./components/LatestCourses"
 export default function App() {
   return (
     <div>
       <Header />
       <Hero />
       <LatestBlog />
+      <Latestcourses/>
     </div>
   )
 }
